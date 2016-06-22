1798b8a4b5c078797ab94765a1d3feaa2bcc4682
Roberto4958 <ccoler@munchwagon.ca>
2016-06-21 22:17:35 -0700

    June21: I changed the type of the parameters in the Locations table. logitude and latitude are now double instead of int

--- database/databaseJune21.sql
+++ database/databaseJune21.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: Finder
 -- ------------------------------------------------------
--- Server version	5.6.27-log
+-- Server version	5.5.43-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,13 +24,13 @@ DROP TABLE IF EXISTS `Locations`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Locations` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `latitude` int(11) NOT NULL,
-  `longitude` int(11) NOT NULL,
-  `userID` int(11) DEFAULT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `userID` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `userID` (`userID`),
   CONSTRAINT `Locations_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `Users` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `Locations` (
 
 LOCK TABLES `Locations` WRITE;
 /*!40000 ALTER TABLE `Locations` DISABLE KEYS */;
-INSERT INTO `Locations` VALUES (3,56,365,1),(4,13,653,3),(5,568,764,1),(6,747,743,2),(7,7356,375,3),(8,357,2246,1),(9,252,5221,3),(10,452,5245,2),(11,452,5245,2),(12,43620,3452,14),(14,-323,3422,15),(15,-323,-989869,15),(16,-323,0,15),(17,43620,0,15);
+INSERT INTO `Locations` VALUES (1,135.53,4394,1),(2,135.53,4394,1),(3,135.53,-48.394,1),(4,-13345.53,94839,2),(5,-1353544,98649,1),(6,-135.4244,389,3),(7,45253,2489,3),(8,13543,24085,3),(9,43.34,-335,1);
 /*!40000 ALTER TABLE `Locations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +59,7 @@ CREATE TABLE `Users` (
   `authToken` varchar(40) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `userName` (`userName`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +68,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'user1','pass1','saly','brown',''),(2,'user2','pass2','Charley','brown',''),(3,'user3','pass3','Mychle','Jordan','9k8oldl3io4eonk1vteaiivjdtz4cd'),(6,'user4','pass4','Aron','Casem',''),(7,'user5','pass5','Albert','Sanchez',''),(9,'user6','pass6','Messi','sanchez',''),(10,'user7','pass7','Peter','Pan',''),(12,'user8','pass7','Peter','Pan',''),(13,'user9','pass9','Spider','Man',''),(14,'user10','pass10','Deniel','Thomas',''),(15,'user11','pass11','Chuck','Noris','f2v0ipxrt8o869p9cmutcjnhpk8vre'),(16,'2553','4314','15','mehulih6haw6bofbdbtpy8zb0aky24','6n7th5axb5rrkmo099i5v0tm8f8g2t'),(18,'user12','pass12','saly','Brown','bo261k4dsc4kn3tjn31z4c7wib4j61');
+INSERT INTO `Users` VALUES (1,'user1','pass1','saly','brown','4w6xdml9hhfje0s0ud9tywgfcgru3g'),(2,'user2','pass2','Charley','brown',''),(3,'user3','pass3','Mychle','Jordan','9k8oldl3io4eonk1vteaiivjdtz4cd'),(6,'user4','pass4','Aron','Casem',''),(7,'user5','pass5','Albert','Sanchez',''),(9,'user6','pass6','Messi','sanchez',''),(10,'user7','pass7','Peter','Pan',''),(12,'user8','pass7','Peter','Pan',''),(13,'user9','pass9','Spider','Man',''),(14,'user10','pass10','Deniel','Thomas',''),(15,'user11','pass11','Chuck','Noris','f2v0ipxrt8o869p9cmutcjnhpk8vre'),(16,'2553','4314','15','mehulih6haw6bofbdbtpy8zb0aky24','6n7th5axb5rrkmo099i5v0tm8f8g2t'),(18,'user12','pass12','saly','Brown','bo261k4dsc4kn3tjn31z4c7wib4j61');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -81,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-21 17:07:34
+-- Dump completed on 2016-06-21 22:11:01
